272c3173e18f28a4edfa3714fcbe8330970ffbaa
Brian Marchand <brian.mrch78@gmail.com>
2021-01-15 16:55:01 +0100

    tp.go

--- tp.go
+++ tp.go
@@ -10,9 +10,15 @@ List := func list(rw http.ResponseWriter, _ *http.Request) {
 	}
 done := func done(rw http.ResponseWriter, _ *http.Request) {
 		io.WriteString(w, "Hello from a HandleFunc("done",done)
+	}
 add := func add(rw http.ResponseWriter, _ *http.Request) {
 		io.WriteString(w, "Hello from a HandleFunc("/add", add)
 	}
 
 http.ListenAndServe(":8080", nil)
+}
+func list(rw http.ResponseWriter, _ *http.Request) {
+    // la fonction list j'ai pas réussi a mettre la slice 
+ID: 0, task "faire les courses"
+ID: 1, task: "Payer les factures"
 }
